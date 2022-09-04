ad4b4199956056b96b03e470916a9e3ecf74680c
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-04 14:48:28 +0200

    Add example

---
+++ Example/Shared/MySwiftPackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftPackageExampleApp.swift
+//  Shared
+//
+//  Created by Kaori Persson on 2022-09-04.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
