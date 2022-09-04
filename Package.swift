ba7a257800f1a40376e566a44ff9201edcd70e61
Studio Kaori <studiokaori.com@gmail.com>
2022-09-04 14:31:24 +0200

    Add: path

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MySwiftPackage",
-            dependencies: ["Starscream"]),
+            dependencies: ["Starscream"],
+            path: "sources"), // target folder path you want to add the dependencies
         .testTarget(
             name: "MySwiftPackageTests",
             dependencies: ["MySwiftPackage"]),
