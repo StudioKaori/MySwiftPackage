97aec6f58cf32b4604ebff962c5bbd18cf06e2a6
Studio Kaori <studiokaori.com@gmail.com>
2022-09-04 14:37:36 +0200

    Add: SwiftUIVeiw

---
+++ Sources/SwiftUIView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kaori Persson on 2022-09-04.
+//
+
+import Foundation
+import SwiftUI
+
+// If there's iOS version limitation, add the following line to the outside of the structure
+//@available(iOS 13.0, *)
+struct SwiftUIView: View {
+    var Body: some View {
+        Text("This is the swiftUIView from the package")
+    }
+}
