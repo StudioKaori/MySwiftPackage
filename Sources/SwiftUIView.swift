fe29815eb91e7b2ea1c8e1664ce95cba5b7c9b17
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-05 10:14:57 +0200

    Update: SwiftUIView

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -1,19 +1,16 @@
 //
-//  File.swift
+//  SwiftUIView.swift
 //  
 //
-//  Created by Kaori Persson on 2022-09-04.
+//  Created by Kaori Persson on 2022-09-05.
 //
 
-import Foundation
 import SwiftUI
 
-// If there's iOS version limitation, add the following line to the inside/outside of the structure
-// depending on the target
-//@available(iOS 13.0, *)
+@available(iOS 13.0, *)
 public struct SwiftUIView: View {
     public init() {}
-    public var Body: some View {
-        Text("This is the swiftUIView from the package")
+    public var body: some View {
+        Text("Hello from the Swift package")
     }
 }
