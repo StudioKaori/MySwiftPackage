f8d6175f53a1c6b8f0fa17470be11f1d9def5c92
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-05 16:15:33 +0200

    Update: image scale

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -12,8 +12,11 @@ public struct SwiftUIView: View {
     public init() {}
     public var body: some View {
         Text("Hello from the Swift package")
+            .padding()
         // Use UIImage to look for the image inside of the package
         // UIImage needs to be unwrap with !
         Image(uiImage: UIImage(named: "starscream", in: .module, with: nil)!)
+            .scaledToFit()
+            .padding()
     }
 }
